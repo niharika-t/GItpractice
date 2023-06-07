819a8ad66582ec29e689883e79ddbede9ec27d8f
niharika-t <tiwarinst6@gmail.com>
2023-06-07 20:35:29 +0530

    Gitproject function commit

---
+++ key_functions/DartFunctions.dart
@@ -0,0 +1,17 @@
+int add(int a,int b)
+{
+    int sum=a+b;
+    return sum;
+}
+int mul(int x,int y)
+{
+  int res=x*y;
+  return res;
+}
+void main()
+{
+  var op=add(10,20);
+  print(op);
+  var op1=mul(20,30);
+  print(op1);
+}
